14ce896c22e2861fc8f02da24b1c9b734e8cd22f
paul <paul2309825@gmail.com>
2024-05-20 10:00:31 +0800

    feat(auth): finish all API but not debug yet

--- api-gateway/auth/app.js
+++ api-gateway/auth/app.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const path = require('path')
-const { verifyJWT, sql_file } = require('./utils')
+const { verifyJWT, sql_file, sql_query, verifyCookie } = require('./utils')
 
 app.use(require('body-parser').json())
 app.use(require('cookie-parser')())
@@ -23,6 +23,63 @@ app.get('/info', verifyJWT, async (req, res) => {
     delete user.passwd;
     res.status(200);
     res.json(user);
+  })
+})
+
+app.patch('/info', verifyJWT, (req, res) => {
+  if (!req.user)
+    return res.sendStatus(401)
+  if (!req.body)
+    return res.sendStatus(400)
+  let paras = []
+  let format = ''
+
+  for (let k in req.body) {
+    if (k != 'name' || k != 'phone' || k != 'profile')
+      return res.sendStatus(400);
+    paras.push(k)
+    paras.push(req.body[k])
+    format += '? = ?, '
+  }
+  if (format.length)
+    format = format.substring(0, format.length - 2)
+  paras.push(req.user.account)
+  sql_query(`update users set ${format} where account = ?`, paras, result => {
+    res.sendStatus(200);
+  })
+})
+
+app.put('/passwd', verifyJWT, (req, res) => {
+  if (!req.user)
+    return res.sendStatus(401)
+  if (!req.body || !req.body.passwd)
+    return res.sendStatus(400)
+  sql_query(`update users set passwd = ? where account = ?`, [req.user.account, res.body.passwd], result => {
+    res.sendStatus(200);
+  })
+})
+
+app.put('/manager/:account', verifyJWT, (req, res) => {
+  if (!req.user)
+    return res.sendStatus(401)
+  if (!req.params || !req.params.account)
+    return res.sendStatus(400)
+  const { account } = req.params;
+  sql_file('sql/manager.sql', [req.user.account, account], result => {
+    if (!result)
+      return res.sendStatus(500);
+    let status_code = result[0].status_code;
+    res.sendStatus(status_code)
+  })
+})
+
+app.get('/users', verifyJWT, (req, res) => {
+  if (!req.user)
+    return res.sendStatus(401)
+  sql_query('select account, email, name, phone, profile manager from users', [req.user.account], result => {
+    if (!result)
+      return res.sendStatus(500);
+    res.json(result)
   })
 })
 
---
+++ api-gateway/auth/sql/manager.sql
@@ -0,0 +1,13 @@
+SET @account = ?, @target = ?;
+
+SELECT 
+  CASE 
+    WHEN EXISTS (SELECT * FROM users WHERE account = @account AND manager = 0) THEN 403
+    WHEN EXISTS (SELECT * FROM users WHERE account = @target) THEN 200
+    ELSE 404
+  END 
+into @status_code;
+
+update users set manager = 1 where account = @target and @status_code = 200;
+
+select @status_code as status_code;
